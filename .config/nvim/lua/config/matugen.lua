f445780a13ce06578f3f78f433cbf4eac2b7a5f4
SlowerTrick <arturvitorcintra@gmail.com>
2026-03-09 18:14:41 -0300

    Updated wallpaper names

--- .config/nvim/lua/config/matugen.lua
+++ .config/nvim/lua/config/matugen.lua
@@ -1,25 +1,25 @@
 local M = {}
 
 M.colors = {
-  base00 = "#1a110f",
-  base01 = "#1a110f",
-  base02 = "#271d1b",
-  base03 = "#534340",
+  base00 = "#16130b",
+  base01 = "#16130b",
+  base02 = "#221f17",
+  base03 = "#4b4739",
 
-  base04 = "#d8c2bd",
-  base05 = "#f1dfdb",
-  base06 = "#f1dfdb",
-  base07 = "#561f12",
+  base04 = "#cdc6b4",
+  base05 = "#e9e2d4",
+  base06 = "#e9e2d4",
+  base07 = "#3a3000",
 
   base08 = "#ffb4ab",
-  base09 = "#f8e1a6",
-  base0A = "#ffdad2",
-  base0B = "#ffb4a3",
+  base09 = "#c6eccd",
+  base0A = "#efe2bc",
+  base0B = "#ddc66e",
 
-  base0C = "#dac58c",
-  base0D = "#ffb4a3",
-  base0E = "#e7bdb3",
-  base0F = "#a08c88",
+  base0C = "#aad0b2",
+  base0D = "#ddc66e",
+  base0E = "#d2c6a1",
+  base0F = "#979080",
 }
 
 return M
